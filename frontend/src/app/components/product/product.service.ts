260ed10934334ef645b36ff125095c781d4e9b17
MarcusWeil <vinicciusplay10@gmail.com>
2022-04-12 17:58:12 -0400

    ErrorHandler
    Lógica de tratamento de erro para quando não for possível realizar a ação solicitada (GET/PUT/DELETE/UPDATE) por algum motivo.
    Adicionado também uma personalização para a Snackbar de sucesso/erro chamado de msg-error e msg-sucess

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, Observable } from 'rxjs';
 import { Product } from './product.model';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,30 +13,42 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 5000,
       horizontalPosition: "right",
       verticalPosition: "top",
+      panelClass: isError ? ['msg-error'] : ['msg-sucess']
     })
   }
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product)
+    // um objeto que vai fazer uma verificação de 'e'
+    return this.http.post<Product>(this.baseUrl, product).pipe(map((obj) => obj), catchError(e => this.errorHandler(e))
+    );
   }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ops, um erro aconteceu', true)
+    return EMPTY;
+  }
+
   read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl)
   }
   readById(id: string | null): Observable<Product> {
     const url = `${this.baseUrl}/${id}` //Guarda a informação da URL base setada lá em cima e concatenando com a ID do produto lido.
-    return this.http.get<Product>(url) //Faz a leitura baseada na informação guardada em URL
+    return this.http.get<Product>(url).pipe(map((obj) => obj), catchError(e => this.errorHandler(e))
+    ); //Faz a leitura baseada na informação guardada em URL
   }
   update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`
-    return this.http.put<Product>(url, product)
+    return this.http.put<Product>(url, product).pipe(map((obj) => obj), catchError(e => this.errorHandler(e))
+    );
   }
   delete(id: string): Observable<Product> {
     const url = `${this.baseUrl}/${id}`
-    return this.http.delete<Product>(url);
+    return this.http.delete<Product>(url).pipe(map((obj) => obj), catchError(e => this.errorHandler(e))
+    );
   }
 }
 
