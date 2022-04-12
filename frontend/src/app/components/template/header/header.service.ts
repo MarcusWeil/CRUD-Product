05c485d45fb46e64c759c1f5a518f8db53d7c3ee
MarcusWeil <vinicciusplay10@gmail.com>
2022-04-12 19:17:45 -0400

    Revert "Revert "HeaderData logic""
    This reverts commit 9ff023052d8816bcc9e8538a46a805cf41c50152.

--- frontend/src/app/components/template/header/header.service.ts
+++ frontend/src/app/components/template/header/header.service.ts
@@ -13,13 +13,13 @@ export class HeaderService {
     icon: 'home',
     routeUrl: ''
   });
-  public get headerData(): HeaderData {
+  get headerData(): HeaderData {
     return this._headerData.value;
   }
   /*headerData vai setar o headerData do tipo "HeaderData.model" para: {
     BehaviorSubject ser capaz de detectar o evento a partir do .next() que estará recebendo o valor do headerData previamente obtido pelo 'get headerData()'
   }*/
-  public set headerData(headerData: HeaderData) {
+  set headerData(headerData: HeaderData) {
     this._headerData.next(this.headerData);
   }
 
